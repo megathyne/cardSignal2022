80064f2dc2897904d98500373304c857b4bf0654
megathyne <megathyne@gmail.com>
2022-06-01 16:39:13 -0400

    initial

--- codeSamples/getSealedData.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-const axios = require("axios");
-
-async function main() {
-  const response = await axios.get("https://api.mtgstocks.com/sealed");
-  await fs.writeFile(`./${new Date().toJSON()}.json`, JSON.stringify(response.data));
-}
-
-main();
---
+++ prototype/src/constants/index.js
@@ -0,0 +1,17 @@
+const SET_BB = "Set Booster Box";
+const SET_BD = "Set Booster Display";
+const DRAFT_BB = "Draft Booster Box";
+const COLLECTOR_BB = "Collector Booster Box";
+const COLLECTOR_BD = "Collector Booster Display";
+const CLASSIC_BB = "Booster Box";
+
+const boxTypes = [
+  SET_BB,
+  SET_BD,
+  DRAFT_BB,
+  COLLECTOR_BB,
+  COLLECTOR_BD,
+  CLASSIC_BB,
+];
+
+module.exports = { boxTypes };
