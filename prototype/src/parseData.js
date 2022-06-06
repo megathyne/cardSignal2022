3bee004dd85b74cb8fe699a0d5449cdebb068e92
megathyne <megathyne@gmail.com>
2022-06-06 18:46:17 -0400

    added types

--- prototype/src/parseData.js
+++ prototype/src/parseData.js
@@ -11,7 +11,7 @@ async function main() {
   // Get all annual anniversary dates for every product till 2022
   masterProducts.map((m) => {
     m.dateRange = [];
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 29; i++) {
       const date = new Date(new Date(m.date).setFullYear(new Date(m.date).getFullYear() + i));
 
       if (date.getFullYear() <= "2022") m.dateRange.push(date);
@@ -28,8 +28,8 @@ async function main() {
 
   // Create a hash table with keys 1-50 to save annual price data
   intervalLookup = {};
-  for (let i = 0; i <= 30; i++) {
-    intervalLookup[i] = [];
+  for (let i = 0; i <= 29; i++) {
+    intervalLookup[i] = { low: [], average: [], high: [], market: [] };
   }
 
   // Calculate needed date per set product
@@ -38,33 +38,48 @@ async function main() {
     const parent = masterProducts.find((mp) => mp.id === parentId);
     const prices = (await mtgData.getProductPrices(slug)).data;
 
-    product.childPrices = [];
+    product.childPrices = { low: [], average: [], high: [], market: [] };
     parent.dateRange.forEach((anniversaryDate, i) => {
       const anniversaryDateUnix = new Date(anniversaryDate).getTime();
-      const anniversaryMarketData = prices.average.find((f) => f[0] === anniversaryDateUnix);
+      const date = anniversaryDate.toJSON().split("T")[0];
 
-      if (anniversaryMarketData) {
-        const data = {
-          year: i,
-          date: new Date(anniversaryMarketData[0]).toJSON().split("T")[0],
-          price: anniversaryMarketData[1],
-          change: ((anniversaryMarketData[1] - msrpLookup[slug]) / msrpLookup[slug]) * 100,
-        };
-        product.childPrices.push(data);
-        intervalLookup[i].push({ slug, change: data.change });
+      const averageData = prices.average.find((f) => f[0] === anniversaryDateUnix);
+      if (averageData) {
+        const price = averageData[1];
+        const change = ((price - msrpLookup[slug]) / msrpLookup[slug]) * 100;
+        const data = { year: i, date, price, change };
+        product.childPrices.average.push(data);
+        intervalLookup[i].average.push({ slug, change: data.change });
+      }
+
+      const lowData = prices.low.find((f) => f[0] === anniversaryDateUnix);
+      if (lowData) {
+        const price = lowData[1];
+        const change = ((price - msrpLookup[slug]) / msrpLookup[slug]) * 100;
+        const data = { year: i, date, price, change };
+        product.childPrices.low.push(data);
+        intervalLookup[i].low.push({ slug, change: data.change });
+      }
+
+      const highData = prices.high.find((f) => f[0] === anniversaryDateUnix);
+      if (highData) {
+        const price = highData[1];
+        const change = ((price - msrpLookup[slug]) / msrpLookup[slug]) * 100;
+        const data = { year: i, date, price, change };
+        product.childPrices.high.push(data);
+        intervalLookup[i].high.push({ slug, change: data.change });
+      }
+
+      const marketData = prices.market.find((f) => f[0] === anniversaryDateUnix);
+      if (marketData) {
+        const price = marketData[1];
+        const change = ((price - msrpLookup[slug]) / msrpLookup[slug]) * 100;
+        const data = { year: i, date, price, change };
+        product.childPrices.market.push(data);
+        intervalLookup[i].market.push({ slug, change: data.change });
       }
     });
-
-    //console.log(parent.name, name, new Date(parent.date));
-    // childPrices.forEach(({ year, price, change }) => {
-    //   if (year === 5) {
-    //     price ? console.log(year, price, change.toFixed(2) + "%") : "";
-    //   }
-    // });
   }
-
-  // console.log(productList[50]);
-  console.log(intervalLookup);
 
   // Show all intervals for each set
   // productList.forEach(({ slug, childPrices }) => {
@@ -79,9 +94,21 @@ async function main() {
 
   // Show the average of sets for each interval
   Object.keys(intervalLookup).map((key) => {
-    const average = intervalLookup[key].reduce((p, c) => (p += c.change), 0) / intervalLookup[key].length;
-    console.log(parseInt(key) + 1, average.toFixed(2) + "%");
+    const average = intervalLookup[key].average.reduce((p, c) => (p += c.change), 0) / intervalLookup[key].average.length;
+    const low = intervalLookup[key].low.reduce((p, c) => (p += c.change), 0) / intervalLookup[key].low.length;
+    const high = intervalLookup[key].high.reduce((p, c) => (p += c.change), 0) / intervalLookup[key].high.length;
+    const market = intervalLookup[key].market.reduce((p, c) => (p += c.change), 0) / intervalLookup[key].market.length;
+    console.log(parseInt(key) + 1, average.toFixed(2) + "%", low.toFixed(2) + "%", high.toFixed(2) + "%", market.toFixed(2) + "%");
   });
+
+  // Sort by change
+  Object.keys(intervalLookup).map((key) => {
+    intervalLookup[key].average.sort((a, b) => (a.change > b.change ? 1 : -1));
+    intervalLookup[key].low.sort((a, b) => (a.change > b.change ? 1 : -1));
+    intervalLookup[key].high.sort((a, b) => (a.change > b.change ? 1 : -1));
+    intervalLookup[key].market.sort((a, b) => (a.change > b.change ? 1 : -1));
+  });
+  console.log(intervalLookup[3]);
 
   // Show the latest market price for each set
   // const boxByPrice = []
